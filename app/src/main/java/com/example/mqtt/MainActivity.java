b48261e434db05221c69be1c74588fdc8f083658
yousif elsekily <yousifelsekily@gmail.com>
2019-05-19 01:16:58 +0200

    add comments

--- app/src/main/java/com/example/mqtt/MainActivity.java
+++ app/src/main/java/com/example/mqtt/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.mqtt;
 
-import android.support.annotation.NonNull;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+
 import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,44 +18,49 @@ import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
-import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
-import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
-
 import java.io.UnsupportedEncodingException;
 
+
+
+/*
+
+source code
+
+                    https://www.hivemq.com/blog/mqtt-client-library-enyclopedia-paho-android-service/
+ */
 
 public class MainActivity extends AppCompatActivity {
 
 
-    String topic = "elsekily/feeds/led";
+    String topic = "elsekily/feeds/led";  //topic name
+    MqttAndroidClient client;  //MQTT client
 
 
-    MqttAndroidClient client;
-    TextView ts;
-    ToggleButton toggle;
+    TextView mqtt_subscribe; //textview
+    ToggleButton toggle; ///button
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ts = (TextView) findViewById(R.id.text);
+        mqtt_subscribe = (TextView) findViewById(R.id.text);
         toggle = (ToggleButton) findViewById(R.id.toggBtn);
 
+        //init button function
         toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
-                    startPublish("1");
+                    startPublish("1");//call this function to publish 1
 
                 } else {
-                    startPublish("0");
-
+                    startPublish("0");//call this function to publish 0
                 }
             }
         });
 
-        String clientId = MqttClient.generateClientId();
-        client = new MqttAndroidClient(this.getApplicationContext(), "tcp://io.adafruit.com:1883", clientId);
+        String clientId = MqttClient.generateClientId();  //new clientid every start
+        client = new MqttAndroidClient(this.getApplicationContext(), "tcp://io.adafruit.com:1883", clientId);  //create the mqtt client
         // Remove if there is no additional options
         MqttConnectOptions options = new MqttConnectOptions();
         options.setUserName("elsekily");
@@ -65,9 +70,9 @@ public class MainActivity extends AppCompatActivity {
             IMqttToken token = client.connect(options);//remove options word if no options
             token.setActionCallback(new IMqttActionListener() {
                 @Override
-                public void onSuccess(IMqttToken asyncActionToken) {
+                public void onSuccess(IMqttToken asyncActionToken) {//case connection to server Succeeded
                     // We are connected
-                    startSubscribe();
+                    startSubscribe();  //call this method after the connection to server Succeeded not before and it will subscribe forever
                     Toast.makeText(MainActivity.this, "onSuccess", Toast.LENGTH_SHORT).show();
                     //Log.d(TAG, "onSuccess");
                 }
@@ -83,7 +88,7 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(MainActivity.this, "cannot connect", Toast.LENGTH_SHORT).show();
         }
 
-        client.setCallback(new MqttCallback() {
+        client.setCallback(new MqttCallback() {  /// callback method of mqtt subscribe
             @Override
             public void connectionLost(Throwable cause) {
 
@@ -91,7 +96,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void messageArrived(String topic, MqttMessage message) throws Exception {
-                ts.setText(message.toString());
+                mqtt_subscribe.setText(message.toString());   // when message received it writes text view with its value
 
             }
 
@@ -104,14 +109,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void startPublish(String s)
+    public void startPublish(String s)  //when button clicked call this method with string s value to publish it
     {
         String payload = s;
-        byte[] encodedPayload = new byte[0];
+        byte[] encodedPayload = new byte[0];  //encoded payload as bytes
         try {
-            encodedPayload = payload.getBytes("UTF-8");
+            encodedPayload = payload.getBytes("UTF-8");  //
             MqttMessage message = new MqttMessage(encodedPayload);
 
+            //message.setRetained(true);   /// to retain message in mqtt broker
             client.publish(topic, message);
         } catch (UnsupportedEncodingException | MqttException e) {
             Toast.makeText(MainActivity.this, "Cannot Publish", Toast.LENGTH_SHORT).show();
@@ -120,7 +126,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void startSubscribe()
+    public void startSubscribe()  // call after connect successfully to the broker
     {
         try {
             IMqttToken subToken = client.subscribe(topic, 1);
